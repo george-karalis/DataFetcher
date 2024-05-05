27a08fa830f0eb48918eaa63f050c57535029c0d
Giorgos <george.karalis@l3harris.com>
2024-05-05 11:41:00 +0100

    Fixes urls format

--- src/tomato_scrapper/urls.py
+++ src/tomato_scrapper/urls.py
@@ -1,9 +1,9 @@
 # Grid URLs
-BROWSE_GRID = "BROWSE_GRID"
+BROWSE_GRID = "https://www.rottentomatoes.com/browse"
 MOVIES_IN_CINEMAS = f"{BROWSE_GRID}/movies_in_theaters"
-MOVIES_AT_HOME = "{BROWSE_GRID}/movies_at_home"
-COMING_SOON = "{BROWSE_GRID}/movies_coming_soon"
-TV_SERIES = "{BROWSE_GRID}/tv_series_browse"
+MOVIES_AT_HOME = f"{BROWSE_GRID}/movies_at_home"
+COMING_SOON = f"{BROWSE_GRID}/movies_coming_soon"
+TV_SERIES = f"{BROWSE_GRID}/tv_series_browse"
 
 # Reviews URLs
 CRITICS = "/reviews"
